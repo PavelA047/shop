800c93be7907f29711c34927dd4167d409b0dba0
pavel <zzzzfogzzzz@gmail.com>
2022-06-14 01:16:45 +0600

    commit_3

---
+++ shopTest/src/test/java/com/example/shoptest/controller/ProductControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.shoptest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+//@AutoConfigureMockMvc
+@SpringBootTest
+class ProductControllerTest {
+
+    @Autowired
+//    private MockMvc mockMvc;
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void addBucket() {
+    }
+
+    @Test
+    void edit() {
+    }
+
+    @Test
+    void processProductAddForm() {
+    }
+}
